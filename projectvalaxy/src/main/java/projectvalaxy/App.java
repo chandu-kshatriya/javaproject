5959dc84d4992b52bdc4078987b406b37f68f612
chandu <chandu.chandra7@gmail.com>
2020-07-10 15:56:02 +0000

    first commit of helloworld

---
+++ projectvalaxy/src/main/java/projectvalaxy/App.java
@@ -0,0 +1,13 @@
+package projectvalaxy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
